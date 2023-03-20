6eb9881052174d470a2a2527fa6fd030e50ee9c0
Toni <tonijepo@gmail.com>
2023-03-20 23:55:19 +0100

    Ejercicio 2 terminado

---
+++ Ejercicio-2/index.js
@@ -0,0 +1,22 @@
+const nombre = "Toni";
+
+const edad = 40;
+
+const mayorDeEdad = true;
+
+const direccion = {
+    calle: 'Rue del Percebe',
+    numero: 13,
+}
+
+
+const coloresFavoritos = ['azul', 'verde'];
+
+const lenguajesFavoritos = ['Javascript', 'Python']
+
+const mejorLenguaje = lenguajesFavoritos[0];
+
+const peorLenguaje = lenguajesFavoritos[1];
+
+
+
