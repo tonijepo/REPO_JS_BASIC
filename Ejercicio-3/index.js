277a8bd9626a0db3138d36174ff03eab17479467
Toni <tonijepo@gmail.com>
2023-03-28 22:55:31 +0200

    Ejercicio 3 terminado

---
+++ Ejercicio-3/index.js
@@ -0,0 +1,19 @@
+const numero1=10;
+const numero2=25;
+
+const suma= numero1 + numero2;
+
+console.log(suma);
+
+const resta= numero1 - numero2;
+
+console.log(resta);
+
+const multiplicacion= numero1 * numero2;
+
+console.log(multiplicacion);
+
+const division= numero1 / numero2;
+
+console.log(division);
+
