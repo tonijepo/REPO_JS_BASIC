8d49de0f2368e352c7c137d68dde2dae028ee633
Toni <tonijepo@gmail.com>
2023-03-28 23:25:32 +0200

    Ejercicio 5 sin usar bucles

---
+++ Ejercicio-5/index.js
@@ -0,0 +1,7 @@
+const pokemons = ['Pikachu', 'Zygarde', 'Meloetta'];
+
+console.log(pokemons[0]);
+console.log(pokemons[1]);
+console.log(pokemons[2]);
+
+console.log(pokemons[0], pokemons[1], pokemons[2]);
