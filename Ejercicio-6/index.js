42748380140c08861f3c1b1c50ef2c82376177a3
Toni <tonijepo@gmail.com>
2023-03-29 23:15:19 +0200

    Ejercicio-6 opción?

---
+++ Ejercicio-6/index.js
@@ -0,0 +1,10 @@
+const pokemons = {
+    nombre: ['Pikachu', 'Zygarde', 'Meloetta'],
+    tipoDePokemon: ['Electrico', 'Tierra', 'Normal']
+}
+
+if (pokemons.tipoDePokemon = 'Tierra') {
+    console.log('Es un pokemon de fuego');
+}
+
+
